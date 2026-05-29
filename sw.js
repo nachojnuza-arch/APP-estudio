d3e2c59ab4f169ef5f4d80471e4f55b7dd1d19e7
nachojnuza-arch <nachojnuza@gmail.com>
2026-05-29 20:30:59 -0300

    13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'apuntes-facultad-cache-v8';
+const CACHE_NAME = 'apuntes-facultad-cache-v9';
 
 // 1. Lista actualizada con la nueva estructura de carpetas
 const FILES_TO_CACHE = [
